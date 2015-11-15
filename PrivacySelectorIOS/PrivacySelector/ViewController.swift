b257300d153e11e64833af164359bad41df21ba8
Wes Delp <wesdelp@me.com>
2015-11-15 00:00:22 -0500

    Season 2: The Lone Label

--- PrivacySelectorIOS/PrivacySelector/ViewController.swift
+++ PrivacySelectorIOS/PrivacySelector/ViewController.swift
@@ -8,10 +8,75 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PrivacyCell : UITableViewCell {
+    @IBOutlet weak var dataLabel: UILabel!
+    @IBOutlet weak var radialButton: UIButton!
+
+    func loadItem(title: String) {
+        dataLabel.text = title
+    }
+}
+
+class RadialCell : UITableViewCell {
+    @IBOutlet weak var radialLabel: UILabel!
+    
+    func loadItem(title: String) {
+        radialLabel.text = title
+    }
+    
+}
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var items: [(String, Bool)] = [
+        ("Data1", true),
+        ("Data2", false),
+        ("Data3", false),
+        ("Data4", false),
+        ("Data5", true)
+    ]
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let (title,isRadialCell) = items[indexPath.row]
+        
+        if isRadialCell {
+            let cell = tableView.dequeueReusableCellWithIdentifier("RadialTableViewCell", forIndexPath: indexPath) as! RadialCell
+            cell.loadItem(title)
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCellWithIdentifier("PrivacyTableViewCell", forIndexPath: indexPath) as! PrivacyCell
+            cell.loadItem(title)
+            cell.radialButton.tag = indexPath.row;
+            cell.radialButton.addTarget(self, action: "radialAction:", forControlEvents: .TouchUpInside)
+            return cell
+        }
+    }
+    
+    @IBAction func radialAction(sender: UIButton) {
+        let titleString = items[sender.tag]
+        print(titleString.0)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.registerNib(UINib(nibName: "RadialTableViewCell", bundle: nil), forCellReuseIdentifier: "RadialTableViewCell")
+        tableView.registerNib(UINib(nibName: "PrivacyTableViewCell", bundle: nil), forCellReuseIdentifier: "PrivacyTableViewCell")
     }
 }
 
