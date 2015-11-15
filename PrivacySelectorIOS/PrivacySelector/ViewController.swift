45b1464bf8aea1ba9eac503e3f276e914cde5b4b
Wes Delp <wesdelp@me.com>
2015-11-15 03:05:00 -0500

    Added buttons and second view

--- PrivacySelectorIOS/PrivacySelector/ViewController.swift
+++ PrivacySelectorIOS/PrivacySelector/ViewController.swift
@@ -14,28 +14,33 @@ class PrivacyCell : UITableViewCell {
 
     func loadItem(title: String) {
         dataLabel.text = title
+        if title == "Complete" {
+            dataLabel.textColor = UIColor.greenColor()
+        }
     }
 }
 
 class RadialCell : UITableViewCell {
     @IBOutlet weak var radialLabel: UILabel!
+    @IBOutlet weak var doneButton: UIButton!
     
-    func loadItem(title: String) {
-        radialLabel.text = title
-    }
+    //func loadItem(title: String) {
+      //  radialLabel.text = title
+    //}
     
 }
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var acceptButton: UIButton!
     
     var items: [(String, Bool)] = [
-        ("Data1", true),
+        ("Data1", false),
         ("Data2", false),
         ("Data3", false),
         ("Data4", false),
-        ("Data5", true)
+        ("Data5", false)
     ]
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -48,7 +53,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         if isRadialCell {
             let cell = tableView.dequeueReusableCellWithIdentifier("RadialTableViewCell", forIndexPath: indexPath) as! RadialCell
-            cell.loadItem(title)
+            cell.doneButton.tag = indexPath.row;
+            cell.doneButton.addTarget(self, action: "doneAction:", forControlEvents: .TouchUpInside)
+            //cell.loadItem(title)
             return cell
         } else {
             let cell = tableView.dequeueReusableCellWithIdentifier("PrivacyTableViewCell", forIndexPath: indexPath) as! PrivacyCell
@@ -61,7 +68,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     @IBAction func radialAction(sender: UIButton) {
         let titleString = items[sender.tag]
-        print(titleString.0)
+        items[sender.tag] = (titleString.0,true)
+        tableView.reloadData()
+    }
+    
+    @IBAction func doneAction(sender: UIButton) {
+        items[sender.tag] = ("Complete",false)
+        tableView.reloadData()
+        
+        for item in items {
+            if item.0 != "Complete" {
+                return
+            }
+        }; acceptButton.enabled = true
+        return
     }
     
     override func awakeFromNib() {
